8d81a74eeeec71a2bd852099779f57e9c24c935a
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-09-11 12:28:17 +0900

    showのビューファイルを作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -47,7 +47,9 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        // $idのデータを取得してビューへ渡す
+        $article = Article::find($id);
+        return view('articles.show', ['article' => $article]);
     }
 
     /**
